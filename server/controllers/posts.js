838e397a2ea9626ef2a7bac0d2e3a9b0265d0677
EccentricSavage <72437171+EccentricSavage@users.noreply.github.com>
2022-05-31 22:26:40 +0200

    Added delete functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import express from "express";
+import res from "express/lib/response.js";
 import mongoose from "mongoose";
 
 import PostMessage from "../models/postMessage.js";
@@ -43,4 +44,17 @@ export const updatePost = async (req, res) => {
   );
 
   res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
+
+  console.log("DELETE");
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted successfully" });
 };
